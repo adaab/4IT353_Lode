7a734e7e76f4111d89a0be002af669414546893d
Tomáš Chour <Tomas.Chour@aspectworks.com>
2019-11-23 13:45:39 +0100

    init game

--- sources/Server/src/logic/Game.java
+++ sources/Server/src/logic/Game.java
@@ -1,9 +1,23 @@
 package logic;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Game {
-    private List<GameField> fields;
+    private ArrayList<GameField> fields;
     private Player playerA;
     private Player playerB;
+
+    public Game() {
+        initGameBoard();
+    }
+
+    private void initGameBoard() {
+        fields = new ArrayList<>();
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        for (int i = 0 ; i < 10 ; i++){
+            for (int j = 1 ; j <= 10 ; j++){
+                this.fields.add(new GameField(letters[i], String.valueOf(j)));
+            }
+        }
+    }
 }
--- sources/Server/src/logic/GameField.java
+++ sources/Server/src/logic/GameField.java
@@ -4,4 +4,8 @@ public class GameField {
     private String x;
     private String y;
 
+    public GameField(String x, String y){
+        this.x = x;
+        this.y = y;
+    }
 }
